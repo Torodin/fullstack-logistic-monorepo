b518d54181077d197a3ca14ad486c6d05ab13146
Diego GL <diegogomezledo@gmail.com>
2026-03-24 18:46:44 +0100

    feat: implement DatabaseModule and PrismaService for database integration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@fullstack-logistic-wrk/prisma'
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/prisma/src/lib/database.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // Export it to use on nestjs apps
+})
+export class DatabaseModule {}
