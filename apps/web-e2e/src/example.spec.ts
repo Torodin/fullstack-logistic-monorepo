dfbb851d534fc3e18a645c3bf5c4665d48016420
Diego GL <diegogomezledo@gmail.com>
2026-03-24 10:47:45 +0100

    feat: add API tests and proxy configuration for development

--- apps/web-e2e/src/example.spec.ts
+++ apps/web-e2e/src/example.spec.ts
@@ -6,3 +6,9 @@ test('has title', async ({ page }) => {
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
 });
+
+test('reach api', async ({ page }) => {
+  const req = await page.request.get('/api');
+
+  expect(req.ok()).toBeTruthy();;
+});
