1261e0a0b7c3804ebd612951731e8ee0cf343f3d
Diego GL <diegogomezledo@gmail.com>
2026-03-25 16:23:36 +0100

    fix: update UpdateShipmentDto to use ApiProperty feat: add state transitions constant

--- apps/api/src/shipments/dto/update-shipment.dto.ts
+++ apps/api/src/shipments/dto/update-shipment.dto.ts
@@ -1,10 +1,10 @@
-import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateShipmentDto } from './create-shipment.dto';
 import { State } from '@fullstack-logistic-wrk/prisma';
 import { IsEnum } from 'class-validator';
 
 export class UpdateShipmentDto extends PartialType(CreateShipmentDto) {
-    @ApiPropertyOptional({
+    @ApiProperty({
         description: 'New state of the shipment',
         enum: State,
         example: State.IN_TRANSIT,
