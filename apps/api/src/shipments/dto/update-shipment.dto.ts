44be5566fdde0ad4adf424119f6a5502cf2c0c15
Diego GL <diegogomezledo@gmail.com>
2026-03-25 20:20:50 +0100

    fix: remove inheritance from CreateShipmentDto in UpdateShipmentDto

--- apps/api/src/shipments/dto/update-shipment.dto.ts
+++ apps/api/src/shipments/dto/update-shipment.dto.ts
@@ -1,9 +1,8 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { CreateShipmentDto } from './create-shipment.dto';
+import { ApiProperty } from '@nestjs/swagger';
 import { State } from '@fullstack-logistic-wrk/prisma';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 
-export class UpdateShipmentDto extends PartialType(CreateShipmentDto) {
+export class UpdateShipmentDto {
     @ApiProperty({
         description: 'New state of the shipment',
         enum: State,
