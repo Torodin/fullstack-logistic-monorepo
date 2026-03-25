bf1f953230f23fb444d82585ccb37de9269e0b9f
Diego GL <diegogomezledo@gmail.com>
2026-03-25 18:08:37 +0100

    feat: add authentication and role-based access control to update shipment endpoint

--- apps/api/src/shipments/shipments.controller.ts
+++ apps/api/src/shipments/shipments.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { ShipmentsService } from './shipments.service';
 import { CreateShipmentDto } from './dto/create-shipment.dto';
 import { UpdateShipmentDto } from './dto/update-shipment.dto';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { RolesGuard } from '../auth/guard/roles.guard';
+import { Role } from '@fullstack-logistic-wrk/prisma';
+import { Roles } from '../auth/decorator/roles.decorator';
 
 @Controller('shipments')
 export class ShipmentsController {
@@ -22,6 +33,10 @@ export class ShipmentsController {
     return this.shipmentsService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Register a new user (SUPERVISOR only)' })
+  @ApiBearerAuth('JWT-auth')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.SUPERVISOR, Role.OPERATOR)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateShipmentDto: UpdateShipmentDto) {
     return this.shipmentsService.update(id, updateShipmentDto);
